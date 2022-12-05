7a83252a48d8ce78de4e2bc5e0374d3dd7bb6c04
duranvr <victorduran89@gmail.com>
2022-12-04 21:04:44 -0300

    finished infinite deck logic

--- main.py
+++ main.py
@@ -23,21 +23,76 @@ def score(hand):
 
     # Counting aces
     for i in range(aces_count):
-        if (tot_points + 10) > 21:
+        if (tot_points + 11) > 21:
             tot_points += 1
         else:
-            tot_points += 10
+            tot_points += 11
 
     return tot_points
 
 
-def continue_game(hand_player, hand_dealer):
-    return score(hand_player) >= 21 or score(hand_dealer) >= 21
+def calc_winner(hand_player, hand_dealer):
+    score_player = score(hand_player)
+    score_dealer = score(hand_dealer)
 
+    if score_player > 21:
+        winner = 'dealer'
+    elif score_dealer > 21:
+        winner = 'player'
+    elif score_player < score_dealer:
+        winner = 'dealer'
+    elif score_player > score_dealer:
+        winner = 'player'
+    else:
+        winner = 'draw'
+
+    return winner
+
+
+def get_player_decision():
+    decision_number = int(input('1: hit, 2: hold'))
+    if decision_number == 1:
+        return 'hit'
+    else:
+        return 'hold'
+
+
+def get_dealer_hits(hand_player, hand_dealer):
+    score_player = score(hand_player)
+    score_dealer = score(hand_dealer)
+
+    dealer_hits = True
+    if score_player > 21:
+        dealer_hits = False
+    elif score_dealer > score_player:
+        dealer_hits = False
+    elif score_dealer == 21:
+        dealer_hits = False
+
+    return dealer_hits
+
+
+def run():
+    hand_player = deal_card() + deal_card()
+    hand_dealer = deal_card() + deal_card()
+
+    print(f'Dealer Hand: [XXX, {hand_dealer[1:]}')
+    current_player_decision = 'hit'
+    while current_player_decision == 'hit' and score(hand_player) < 21:
+        print(f'Player Hand: {hand_player}')
+        current_player_decision = get_player_decision()
+        if current_player_decision == 'hit':
+            hand_player += deal_card()
+
+    while get_dealer_hits(hand_player, hand_dealer):
+        hand_dealer += deal_card()
+
+    print(f'Final Player Hand: {hand_player}')
+    print(f'Player Score: {score(hand_player)}')
+    print(f'Final Dealer Hand: {hand_dealer}')
+    print(f'Dealer Score: {score(hand_dealer)}')
+    print(f'Winner is: {calc_winner(hand_player, hand_dealer)}')
 
 if __name__ == '__main__':
-    hand_player = []
-    hand_dealer = []
+    run()
 
-    hand_player += deal_card()
-    # while continue_game(hand_player, hand_dealer)
